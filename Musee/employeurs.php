a64d25e1d23a77bc5b927358ca71389adca605ee
eldelmira <109690631+eldelmira@users.noreply.github.com>
2025-03-11 21:54:02 +0100

    Add files via upload

---
+++ Musee/employeurs.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Musée</title>
+</head>
+<body>
+    <div class="container" id="employeurs">
+        <h2>Section Employés</h2>
+        <p>Accédez aux ressources et informations dédiées aux employés du musée.</p>
+        <div class="login-form" id="login-form">
+            <h3>Connexion</h3>
+            <input type="text" id="username" placeholder="Nom d'utilisateur">
+            <input type="password" id="password" placeholder="Mot de passe">
+            <button onclick="login()">Se connecter</button>
+        </div>
+
+</body>
+</html>
